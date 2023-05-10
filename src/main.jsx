af3db8369f7f97ca9f6eeff2f53fa326b7b17be1
Shuvo Deb <shuvodeb17014@gmail.com>
2023-05-10 08:43:08 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './routes/route.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className='max-w-6xl	mx-auto'>
+    <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  </div>,
 )
